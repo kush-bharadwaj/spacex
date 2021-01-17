45be09cdb03976c28b26d1f22091678888c6fb1e
kush-bharadwaj <kush.bharadwaj@rsystems.com>
2021-01-17 15:39:01 +0530

    'spacex'

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,10 @@ class IndexPage extends Component {
     componentDidMount() {
         this.props.router.push('/home/dashboard');
     }
-    // componentDidUpdate(prevprops, nextprops){
-    //     console.log(prevprops.router);
-    //     console.log(nextprops.router);
-    // }
 
     render() {
         return (
-            <div>
-                
-            </div>
+            <div> </div>
         );
     }
 }
